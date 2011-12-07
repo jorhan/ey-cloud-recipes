339a20fef6c5fe912c677166b04c97aa8d3b6467
Jor Han Lau <jorhan.lau@gmail.com>
2011-12-06 18:48:27 -0800

    fix recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,8 @@
 #  }
 #end
 
+require_recipe "statwatcher"
+
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
 # require_recipe "sphinx"
 
--- cookbooks/statwatcher/recipes/default.rb
+++ cookbooks/statwatcher/recipes/default.rb
@@ -1,34 +1,41 @@
 require 'pp'
 #
-# Cookbook Name:: memcached
+# Cookbook Name:: daemon services
 # Recipe:: default
 #
-utility_name = "BackendProcess"
 
 node[:applications].each do |app_name,data|
   user = node[:users].first
 
-case node[:instance_role]
- when "solo", "app", "app_master"
-   template "/var/services/stats_watcher/run" do
-     source "run.erb"
-     owner user[:username]
-     group user[:username]
-     mode 0744
-     variables({
-         :app_name => app_name,
-         :rails_env => node[:environment][:framework_env] 
-     })
-   end
+  if ['util'].include?(node[:instance_role])
+     template "/var/services/stats_watcher/run" do
+       source "run.erb"
+       owner user[:username]
+       group user[:username]
+       mode 0744
+       variables({
+           :app_name => app_name,
+           :rails_env => node[:environment][:framework_env] 
+       })
+     end
+     
+     template "/var/services/stats_watcher/log/run" do
+       source "run_log.erb"
+       owner user[:username]
+       group user[:username]
+       mode 0744
+     end    
 
-   link "/service/stats_watcher" do
-     to "/var/service/stats_watcher"
-   end    
- end
-end
-if node[:name] == utility_name
-  run_for_app(appname) do |app_name, data|
-    execute "svc -k /service/stats_watcher" do
+     link "/service/stats_watcher" do
+       to "/var/services/stats_watcher"
+     end    
+
+    run_for_app(app_name) do |app_name, data|
+      execute "svc /service/stats_watcher" do
+      end
     end
+
   end
+  
+
 end
