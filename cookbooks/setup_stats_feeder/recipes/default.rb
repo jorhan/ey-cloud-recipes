8263f07ffe5c98e87aeaac9a1ae29cb4a258af25
Jor Han Lau <jorhan.lau@gmail.com>
2011-12-13 23:11:54 -0800

    stable

--- cookbooks/setup_stats_feeder/recipes/default.rb
+++ cookbooks/setup_stats_feeder/recipes/default.rb
@@ -28,30 +28,28 @@ node[:applications].each do |app_name,data|
 
     #TODO : this command fail in build need to fix it
    
-    if File.exists?("/data/${app_name}/current")
-      unless File.exists?("/data/${app_name}/FeedFetcherDeluxe")
+    if File.exists?("/data/todo/current")
+      unless File.exists?("/data/todo/FeedFetcherDeluxe")
         run_for_app(app_name) do |app_name, data|
           execute "extract files" do
-            command "cd /data/${app_name} && tar xzvf /data/${app_name}/current/lib/feedfetcher.tar.gz"
+            command "cd /data/todo && tar xzvf /data/todo/current/lib/feedfetcher.tar.gz"
           end
 
         end
       end
 
-      template "/data/${app_name}/FeedFetcherDeluxe/conf/feedfetcher.cfg" do
+      template "/data/todo/FeedFetcherDeluxe/conf/feedfetcher.cfg" do
         source "feedfetcher.erb"
         owner 'deploy'
         group 'deploy'
         mode 0744
       end
       
-      if File.exists?("/data/${app_name}/FeedFetcherDeluxe")
-        #create folder where the xml are located
-        unless File.exists?("/data/${app_name}/FeedFetcherDeluxe/working")
-           Dir::mkdir("/data/${app_name}/FeedFetcherDeluxe/working")
-        end           
-        
-      end
+      #create folder where the xml are located
+      unless File.exists?("/data/todo/FeedFetcherDeluxe/working")
+         Dir::mkdir("/data/todo/FeedFetcherDeluxe/working")
+      end           
+
       
     end
   end  
